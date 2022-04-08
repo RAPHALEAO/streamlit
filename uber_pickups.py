41023d04ccde1d61f3696b9aa8630d245684b9c9
Raphael Darce Leão <30324186+RAPHALEAO@users.noreply.github.com>
2022-04-08 17:41:19 -0300

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -8,16 +8,14 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+@st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
-  
-@st.cache
-def load_data(nrows):
-         
+
   # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
