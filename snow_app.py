af3d8c639cd32fc2fed9260285f61b6cfd0e1736
Raphael Darce Leão <30324186+RAPHALEAO@users.noreply.github.com>
2022-04-28 11:53:36 -0300

    Update snow_app.py

--- snow_app.py
+++ snow_app.py
@@ -46,11 +46,11 @@ x3 = df[2]
 # Group data together
 hist_data = [x1, x2, x3]
 
-group_labels = ['Group 1', 'Group 2', 'Group 3']
+group_labels = ['Quantidade', 'Mês', 'Ano']
 
 # Create distplot with custom bin_size
 fig = ff.create_distplot(
-         hist_data, group_labels, bin_size=[.1, .25, .5])
+         hist_data, group_labels)
 
 # Plot!
 st.plotly_chart(fig, use_container_width=True)
