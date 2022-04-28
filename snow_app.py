7ecd04178f1375cd135f5713e8c954fb4e3c7ce2
Raphael Darce Leão <30324186+RAPHALEAO@users.noreply.github.com>
2022-04-28 13:51:13 -0300

    Update snow_app.py

--- snow_app.py
+++ snow_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 import numpy as np
-import plotly as px
+import plotly.express as px
 import matplotlib.pyplot as plt
 import matplotlib
 
