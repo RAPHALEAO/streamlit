674be11f0dbfad72dfe653f577b25b637e3dd31d
Raphael Darce Leão <30324186+RAPHALEAO@users.noreply.github.com>
2022-04-28 11:33:49 -0300

    Update snow_app.py

--- snow_app.py
+++ snow_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
