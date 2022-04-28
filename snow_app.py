929cff8caf1abf78b02687eddee12b47e5c23e18
Raphael Darce Leão <30324186+RAPHALEAO@users.noreply.github.com>
2022-04-28 10:33:29 -0300

    Update snow_app.py

--- snow_app.py
+++ snow_app.py
@@ -29,7 +29,9 @@ rows = run_query("SELECT COUNT(1) AS QTDE, TO_VARCHAR(MONTH(DATAHORA)) AS MONTH,
 
 df = pd.DataFrame(rows)
 
+st.header("EVOLUÇÃO DE PEDIDOS DO TRADE")
+
 if st.checkbox('Show raw data'):
-    st.header("EVOLUÇÃO DE PEDIDOS DO TRADE")
+    
     st.write(df)
 
