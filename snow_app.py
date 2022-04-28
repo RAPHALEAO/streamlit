3272944c77e0c84521f5965aeecf26a034462291
Raphael Darce Leão <30324186+RAPHALEAO@users.noreply.github.com>
2022-04-28 12:09:43 -0300

    Update snow_app.py

--- snow_app.py
+++ snow_app.py
@@ -43,6 +43,7 @@ x = df[1]
 
 fig = go.Figure()
 fig.add_trace(go.Bar(x=x, y=df[0]))
+fig.add_trace(go.Bar(x=x, y=df[2]))
 
 fig.update_layout(barmode='relative', title_text='Relative Barmode')
 fig.show()
